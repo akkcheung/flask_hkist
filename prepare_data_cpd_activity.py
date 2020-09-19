367d8be432556f51ae5e36935e64b0af5165ec6e
Alex Cheung <alexkk.cheung@gmail.com>
2020-09-20 00:10:49 +0800

    generate empty cpd form

--- prepare_data_cpd_activity.py
+++ prepare_data_cpd_activity.py
@@ -13,83 +13,54 @@ csvData_records = CsvData.query.all()
 
 for csvData in csvData_records :
 
-    ''' 
-    print(csvData.quali_issue_yr_a)
-    print (csvData.quali_name_a)
-    print (csvData.quali_issue_auth_a)
-    '''
-
     user = User.query.filter_by(email = csvData.email).first()
 
     if user is None :
         pass
-        '''
-        user = User(email = csvData.email)
-        user.set_password(password)
-        db.session.add(user)
-        '''
         
-
     else:
-
         personDetail = PersonDetail.query.filter_by(user_id = user.id).first()
     
         if personDetail :
 
-            #personDetail.online_id = csvData.online_id
-            #personDetail.old_id = csvData.id
-            personDetail.date_of_approve = date.today()
+            #print(csvData.email)
+            #print(user.id)
 
-            #print(csvData.id)
-            #print(personDetail.old_id)
+            if not personDetail.date_of_approve :
+                personDetail.date_of_approve = date.today()
 
-            print(csvData.email)
-            print(user.id)
-
-            db.session.commit()
+                db.session.commit()
     
-            cpdActivityEntryHeader = CpdActivityEntryHeader.query.filter_by(user_id = user.id).order_by(CpdActivityEntryHeader.id.desc()).first()
+            if personDetail.is_register :
+                cpdActivityEntryHeader = CpdActivityEntryHeader.query.filter_by(user_id = user.id).order_by(CpdActivityEntryHeader.id.desc()).first()
 
-            '''
-            if cpdActivityEntryHeader : 
-                if cpdActivityEntryHeader.end_date > date.today() :
-                    flash('No annual membership fee is overdue yet!', 'warning')
-                    return redirect(url_for('payment'))
-            '''
 
-            '''
-            if cpdActivityEntryHeader:
-                cpdActivityEntryHeader.payment_id = paymentHistory.id
-                cpdActivityEntryHeader.is_closed = True
+                if not cpdActivityEntryHeader:
+                    cpdActivityEntryHeader_new = CpdActivityEntryHeader()
+                    #cpdActivityEntryHeader_new.start_date = date.today()
+                    cpdActivityEntryHeader_new.start_date = datetime(2020,1,1)
+                    #cpdActivityEntryHeader_new.end_date = date.today() + relativedelta(years=1) - timedelta(days=1)
+                    cpdActivityEntryHeader_new.end_date = datetime(2020,1,1) + relativedelta(years=1) - timedelta(days=1)
 
-            cpdActivityEntryHeader_new = CpdActivityEntryHeader()
-            '''
-            
+                    cpdActivityEntryHeader_new.user_id = user.id
 
-            if not cpdActivityEntryHeader:
-                cpdActivityEntryHeader_new = CpdActivityEntryHeader()
-                #cpdActivityEntryHeader_new.start_date = date.today()
-                cpdActivityEntryHeader_new.start_date = datetime(2020,1,1)
-                #cpdActivityEntryHeader_new.end_date = date.today() + relativedelta(years=1) - timedelta(days=1)
-                cpdActivityEntryHeader_new.end_date = datetime(2020,1,1) + relativedelta(years=1) - timedelta(days=1)
+                    db.session.add(cpdActivityEntryHeader_new)
+                    db.session.commit()
 
-                cpdActivityEntryHeader_new.user_id = user.id
+                    for i in range(1, 10):
 
-                db.session.add(cpdActivityEntryHeader_new)
-                db.session.commit()
+                        cpdActivityEntry = CpdActivityEntry()
 
-                for i in range(1, 10):
+                        cpdActivityEntry.cpd_activity_id = i
+                        #cpdActivityEntry.year = year
+                        #cpdActivityEntry.user_id = current_user.id
+                        cpdActivityEntry.point_awarded = 0
 
-                    cpdActivityEntry = CpdActivityEntry()
+                        cpdActivityEntry.cpd_activity_entry_header_id = cpdActivityEntryHeader_new.id
 
-                    cpdActivityEntry.cpd_activity_id = i
-                    #cpdActivityEntry.year = year
-                    #cpdActivityEntry.user_id = current_user.id
-                    cpdActivityEntry.point_awarded = 0
+                        db.session.add(cpdActivityEntry)
 
-                    cpdActivityEntry.cpd_activity_entry_header_id = cpdActivityEntryHeader_new.id
+                    db.session.commit()
 
-                    db.session.add(cpdActivityEntry)
-                    #db.session.commit()
+                    print("{email}, user id:{user_id}, cpd header id :{cpd_id}".format(email=csvData.email, user_id=user.id, cpd_id=cpdActivityEntryHeader_new.id))
 
-                db.session.commit()
